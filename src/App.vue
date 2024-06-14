60b84ebe6c01a430ec5dd79396b636d4b74c3212
VitoMarino <marino.vito2001@gmail.com>
2024-06-14 11:20:54 +0200

    Inizio progetto BOOLFLIX

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components:{
+    AppHeader,
+    AppMain
+  }
+}
+</script>
+
+<template>
+  <AppHeader/>
+
+  <AppMain/>
+</template>
+
+<!--Se non inserisco lo scope TUTTE le regole saranno applicate a TUTTA la mia applicazione. 
+In questo caso è quello che voglio dato che questa (APP.Vue) è la madre di tutte-->
+
+<!--QUESTO VALE PER IL GENERAL. Se voglio usare le PARTIALS, devo importarlo nel singolo file
+dove voglio venga applicata quella regola. Altrimenti non lo importa.-->
+<style lang="scss">
+  @use './styles/general.scss';
+</style>
+
