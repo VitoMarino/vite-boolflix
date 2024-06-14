8d20780318a113d63197731c61b17e3fbd4916c8
VitoMarino <marino.vito2001@gmail.com>
2024-06-14 17:43:06 +0200

    API flag

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -12,14 +12,30 @@ export default {
             search:{
 
             },
+
+            flag:{
+
+            },
         }
     },
     methods:{
+        //API Search Movies
         getSearch(ricerca){
             axios.get('https://api.themoviedb.org/3/search/movie?api_key=93df66e930b448fe4ba34c7efeb9d6b0&query=' + ricerca)
             .then((response) => {
                 this.search = response.data.results;
                 console.log(this.search);
+            })
+            .catch(function(error) {
+            console.log(error);
+            })
+        },
+        // API flag
+        getFlag(language){
+            axios.get('https://flagcdn.com/20x15/it.png' + language + ".png")
+            .then((response) => {
+                this.fleg = response;
+                console.log(this.fleg);
             })
             .catch(function(error) {
             console.log(error);
