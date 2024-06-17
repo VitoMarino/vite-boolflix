9ae556c1709b128d543b0d56620bfb22d3c4eee8
VitoMarino <marino.vito2001@gmail.com>
2024-06-17 13:02:08 +0200

    Revert "API flag"
    This reverts commit 8d20780318a113d63197731c61b17e3fbd4916c8.

--- src/components/MainSearch.vue
+++ src/components/MainSearch.vue
@@ -4,7 +4,11 @@ import {store} from '../store.js';
 export default {
     data() {
         return {
+<<<<<<< HEAD
             store,
+=======
+        testo: "",
+>>>>>>> parent of 8d20780 (API flag)
         }
     },
     props:{
@@ -18,7 +22,11 @@ export default {
 <template>
     <main>
         <button @click="$emit('searchButton', testo)">CLICK</button>
+<<<<<<< HEAD
         <input v-model="store.testo" type="text">
+=======
+        <input v-model="testo" type="text">
+>>>>>>> parent of 8d20780 (API flag)
     </main>
 </template>
 
