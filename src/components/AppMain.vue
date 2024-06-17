0c3560864453658956fa435de672fbad0cba8a22
VitoMarino <marino.vito2001@gmail.com>
2024-06-17 11:22:38 +0200

    Componenti

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -13,31 +13,15 @@ export default {
     },
     data() {
         return {
-            search:{
+            movie:{
 
             },
-
-            flag:{
-
-            },
-
             searchSeries:{
 
             }
         }
     },
     methods:{
-        //API Search Movies
-        getSearch(ricerca){
-            axios.get('https://api.themoviedb.org/3/search/movie?api_key=93df66e930b448fe4ba34c7efeb9d6b0&query=' + ricerca)
-            .then((response) => {
-                this.search = response.data.results;
-                console.log(this.search);
-            })
-            .catch(function(error) {
-            console.log(error);
-            })
-        },
         
         // API Series
         getSearchSeries(ricerca){
@@ -65,8 +49,7 @@ export default {
         // BUTTON
         searchButton(text){
             console.log(text)
-            this.getSearch(text)
-            this.getSearchSeries(text)
+            this.getSearchMovie(text)
         }
     }
 }
@@ -74,24 +57,11 @@ export default {
 
 <template>
     <main>
-        <MainSearch @searchButton="searchButton" />
-        <MainSeries @searchButton="searchButton"/>
-        <MainMovie/>
-        <ul v-for="search in search" :key="search.id">
-            <li>
-                {{ search.title }}
-            </li>
-            <li>
-                {{ search.original_title }}        
-            </li>
-            <!--RITORNARE PER CAPIRE COME GESTIRE CON API-->
-            <li>
-                <img class="img-flag" src="../img/gb.png" alt="img">
-            </li>
-            <li>
-                {{ search.vote_average }}
-            </li>
-        </ul>
+        <MainSearch @searchButton="searchButton"/>
+        <MainMovie :movie="movie"/>
+        <MainSeries/>
+        
+        
     </main>
 </template>
 
--- src/components/MainMovie.vue
+++ src/components/MainMovie.vue
@@ -2,18 +2,48 @@
 export default {
     data() {
         return {
-
+            searchMovie:{
+            },
         }
     },
+    props:{
+        movie: Object,
+    },
     methods:{
-
+        //API Search Movies
+        getSearchMovie(ricerca){
+            axios.get('https://api.themoviedb.org/3/search/movie?api_key=93df66e930b448fe4ba34c7efeb9d6b0&query=' + ricerca)
+            .then((response) => {
+                this.searchMovie = response.data.results;
+                console.log(this.search);
+            })
+            .catch(function(error) {
+            console.log(error);
+            })
+        },
     },
     
 }
 </script>
 
 <template>
-
+    <main>
+        <ul v-for="searchMovie in searchMovie" :key="searchMovie.id">
+            <li>
+                {{ searchMovie.title }}
+            </li>
+            <li>
+                {{ searchMovie.original_title }}        
+            </li>
+            <!--RITORNARE PER CAPIRE COME GESTIRE CON API-->
+            <li>
+                <img class="img-flag" src="../img/gb.png" alt="img">
+            </li>
+            <li>
+                {{ searchMovie.vote_average }}
+            </li>
+        </ul>
+    </main>
 </template>
 
 <style lang="scss" scoped>
