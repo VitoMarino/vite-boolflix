08aef3686c11a671eba512f04af30d639d10f8a4
VitoMarino <marino.vito2001@gmail.com>
2024-06-17 13:12:38 +0200

    Commit

--- src/components/MainSearch.vue
+++ src/components/MainSearch.vue
@@ -4,12 +4,8 @@ import {store} from '../store.js';
 export default {
     data() {
         return {
-<<<<<<< HEAD
             store,
-=======
-        testo: "",
->>>>>>> parent of 8d20780 (API flag)
-        }
+}
     },
     props:{
         search: Object
@@ -22,12 +18,8 @@ export default {
 <template>
     <main>
         <button @click="$emit('searchButton', testo)">CLICK</button>
-<<<<<<< HEAD
         <input v-model="store.testo" type="text">
-=======
-        <input v-model="testo" type="text">
->>>>>>> parent of 8d20780 (API flag)
-    </main>
+</main>
 </template>
 
 <style lang="scss" scoped>
